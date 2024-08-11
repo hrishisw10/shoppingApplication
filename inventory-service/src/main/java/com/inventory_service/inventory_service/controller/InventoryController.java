fcaa91b3538400dfd02e6a29b92b917735803e2c
Hrishikesh <hrishisw10@gmail.com>
2024-08-11 11:50:03 +0530

    Inventory Service to keep track of all the products and its availability

---
+++ inventory-service/src/main/java/com/inventory_service/inventory_service/controller/InventoryController.java
@@ -0,0 +1,25 @@
+package com.inventory_service.inventory_service.controller;
+
+import com.inventory_service.inventory_service.dto.InventoryResponse;
+import com.inventory_service.inventory_service.service.InventoryService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/inventory")
+@RequiredArgsConstructor
+public class InventoryController {
+
+    private final InventoryService inventoryService;
+
+    // http://localhost:8082/api/inventory?skuCode=iphone-13&skuCode=iphone-13-red
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<InventoryResponse> isInStock(@RequestParam List<String> skuCode){
+
+        return inventoryService.isInStock(skuCode);
+    }
+}
